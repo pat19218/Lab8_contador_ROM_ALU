80c3745070a697d967b3dd992fafd45ea48f5c7d
pat19218 <68085020+pat19218@users.noreply.github.com>
2020-10-22 16:10:59 -0600

    bitte

--- Ejercicio 1/contador.v
+++ Ejercicio 1/contador.v
@@ -1,11 +1,17 @@
 module counter #(parameter N=12)
 		(input logic clk, 
 		 input logic reset,
+		 input logic load,
+		 input logic enabled,
+		 input [11:0] d,
 		 output logic [N-1:0]q);
 
 always@(posedge clk, posedge reset)
 	if(reset)
 		q<=0;
-	else
+	else if (load)
+		q<=d;
+	else if (enabled)
 		q<=q+1;
+
 endmodule
--- Ejercicio 1/contador_tb.v
+++ Ejercicio 1/contador_tb.v
@@ -2,12 +2,24 @@ module testbench();
 
 reg clk = 0;
 reg reset = 1;
-
+reg load = 0;
+reg enabled = 0;
+reg [11:0]data;
 output wire[11:0]q;
 
-counter c1(clk, reset, q);
-initial
+
+counter c1(clk, reset, load, enabled, data, q);
+
+initial begin
 	#1 reset = 0;
+	#5 enabled = 1;
+	#5 data = 12'b000000111000;
+	#1 load = 1; #1 load = 0; 
+	#5 enabled = 0;
+	#6 enabled = 1; data = 12'b000100110000;
+	#4 load = 1; #1 load = 0;
+	
+end
 
 
 
