30132ebed0bb82dbc42323df526918ce3fbfc8d5
pat19218 <68085020+pat19218@users.noreply.github.com>
2020-10-23 17:22:57 -0600

    modulo listo
    ALU creado el punto "v" se creo sin problemas

--- Ejercicio 3/ALU.v
+++ Ejercicio 3/ALU.v
@@ -1,12 +1,39 @@
-module memory (input wire [11:0]addres, output wire [7:0]data);
+module ALU (input wire [3:0] A,B, 
+	    input wire [2:0] funcion, 
+	    output logic[3:0] resul);
 
-	reg [7:0]mem[0:4095];	
+reg[3:0] Alu_resultado;
+parameter f1 = 3'b000;
+parameter f2 = 3'b001;
+parameter f3 = 3'b010;
+parameter f4 = 3'b011;
+parameter f5 = 3'b100;
+parameter f6 = 3'b101;
+parameter f7 = 3'b110;
+parameter f8 = 3'b111;
 
-initial begin
-	$readmemb("memoria.list",mem);
+always@(*) begin
+	case(funcion)
+		f1: 
+			Alu_resultado = A & B;
+		f2: 
+			Alu_resultado = A | B;
+		f3: 
+			Alu_resultado = A + B;
+		f4: 
+			Alu_resultado = 4'b0000;
+		f5: 
+			Alu_resultado = A & ~B;
+		f6: 
+			Alu_resultado = A | ~B;
+		f7: 
+			Alu_resultado = A - B;
+		f8: 
+			Alu_resultado = (A<B)? 4'd1:4'd0 ;
+
+		default: Alu_resultado = A + B;
+	endcase
+	assign resul = Alu_resultado;
 end
-
-assign data = mem[addres];
-
 endmodule
 
